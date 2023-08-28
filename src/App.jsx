5b7d343e59e0855bc30518b9ff99c0773ce47e28
Doğukan <dgkn_okms2011@hotmail.com>
2023-08-28 17:21:47 +0300

    some changes

--- src/App.jsx
+++ src/App.jsx
@@ -28,12 +28,13 @@ function Logo() {
 
 function Form() {
 
-  function handleSubmit() {
+  function handleSubmit(e) {
+    e.preventDefault();
 
   }
   
   return (
-    <form className="add-form" onSubmit={handleSubmit}>
+    <form className="add-form" onSubmit={(e) => handleSubmit(e)}>
     <h3>What do you need for your 😍 trip?</h3>
     <select name="" id="">
       {Array.from({length: 20}, (_, i) => i + 1).map(num=> <option value={num} key={num}></option>)}
